0f2adab899c4ef3e7f8417461a248961ee3e4cba
Sajid <saajiidi@gmail.com>
2026-02-23 11:02:32 +0600

    PWA Fix: Enabled dev mode and added required app icons

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ export default defineConfig({
         react(),
         VitePWA({
             registerType: 'autoUpdate',
+            devOptions: {
+                enabled: true,
+                type: 'module'
+            },
             includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
             manifest: {
                 name: 'Ramadan Compass',
@@ -18,19 +22,9 @@ export default defineConfig({
                 orientation: 'portrait',
                 icons: [
                     {
-                        src: 'pwa-192x192.png',
-                        sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'pwa-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
+                        src: 'mask-icon.svg',
+                        sizes: 'any',
+                        type: 'image/svg+xml',
                         purpose: 'any maskable'
                     }
                 ]
